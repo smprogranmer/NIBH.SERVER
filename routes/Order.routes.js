95fabd5e61675957a5b24f1a9956a6bbd03d7a97
siyam <s.mprogrammer788@gmail.com>
2025-01-15 04:05:09 -0800

    try

--- routes/Order.routes.js
+++ routes/Order.routes.js
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { createOrder, getAllOrder,  getOrdersByUserRefId } from '../controllers/Order.controller.js';
+// import { createOrder,,  getOrdersByUserRefId } from '../controllers/Order.controller.js';
+import {  getAllOrder} from '../controllers/Order.controller.js';
 
 const router = Router();
 
 router.get('/',getAllOrder)
-router.get('/my-orders',getOrdersByUserRefId)
-router.post('/create-order',createOrder)
+// router.get('/my-orders',getOrdersByUserRefId)
+// router.post('/create-order',createOrder)
 
 
 export default router
