b3f59f418c15454ef8c91e475350ddbba1dc3b24
siyam <s.mprogrammer788@gmail.com>
2025-01-16 01:56:48 -0800

    push order route

--- routes/Order.routes.js
+++ routes/Order.routes.js
@@ -1,13 +1,11 @@
 import { Router } from 'express';
-import { getOrdersByUserRefId } from '../controllers/Order.controller.js';
-// import { createOrder,,  getOrdersByUserRefId } from '../controllers/Order.controller.js';
-// import {  getAllOrder} from '../controllers/Order.controller.js';
+import { createOrder, getAllOrder,  getOrdersByUserRefId } from '../controllers/Order.controller.js';
 
 const router = Router();
 
-router.get('/',getOrdersByUserRefId)
-// router.get('/my-orders',getOrdersByUserRefId)
-// router.post('/create-order',createOrder)
+router.get('/',getAllOrder)
+router.get('/my-orders',getOrdersByUserRefId)
+router.post('/create-order',createOrder)
 
 
 export default router
